7295966dff1eb147171a7b337a2e2ced3606f0fc
argeulimbo <argeusousaptbr@gmail.com>
2025-04-30 15:10:08 -0300

    init: iniciando projeto -Java RESTful API

---
+++ src/main/java/dio/lint/DioBradescoJavaCloudNativeApplication.java
@@ -0,0 +1,13 @@
+package dio.lint;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DioBradescoJavaCloudNativeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DioBradescoJavaCloudNativeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dio/lint/DioBradescoJavaCloudNativeApplicationTests.java
@@ -0,0 +1,13 @@
+package dio.lint;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DioBradescoJavaCloudNativeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
